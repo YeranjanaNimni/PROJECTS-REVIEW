c2fddf998413323ae96877189e4f10694c7b4b8d
Nimni Almeida <yeranjananimni10@gmail.com>
2021-11-21 19:04:30 +0530

    single select mcq question created

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,28 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+
+  public q1Answers = [
+    {index: 'A', value:'-4'},
+    {index: 'B', value:'-3'},
+    {index: 'C', value:'4'},
+    {index: 'D', value:'7'},
+    {index: 'E', value:'12'}
+  ];
+
+
   constructor() {}
 
+  selectedAnswer(id: any){
+    for(const each of this.q1Answers){
+      document.getElementById(each.index).setAttribute('style','border:none');
+    }
+    if(id === 'C'){
+      document.getElementById(id).setAttribute('style','border:1px solid green');
+      document.getElementById('single-select').setAttribute('style','border:1px solid green');
+    } else{
+      document.getElementById(id).setAttribute('style','border:1px solid red');
+      document.getElementById('single-select').setAttribute('style','border:1px solid red');
+    }
+  }
 }
